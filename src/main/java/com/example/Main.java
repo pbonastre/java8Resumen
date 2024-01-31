2e0d06e25b4c29a4bc4d9fb324a1ce6292fb62f1
pbonastre <bonastrep@gmail.com>
2024-01-31 19:11:01 +0100

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,38 @@
 package com.example;
 
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+public static void main(String[] args) {
+
+        System.out.println(changeFormat("please quote  your policy number:112-39-8552."));
+        System.out.println(readFirstLine("src/main/resources/prueba.txt"));
+        ;
+    }
+
+    public static String changeFormat(String paragraph) {
+
+        return Pattern.compile("\\d{3}-\\d{2}-\\d{4}").matcher(paragraph)
+                .replaceAll(mr -> mr.group().replaceAll("-", "/"));
+    }
+
+    static String readFirstLine(String path){
+        BufferedReader file = null;
+        String buffer = null;
+
+        try{
+            file = new BufferedReader(new FileReader(path));
+            buffer = file.readLine();
+
+        }catch (IOException e) {
+            System.out.println("CAGADA");
+        }finally {
+            if(file !=null){
+                try{
+                    file.close();
+                }catch (IOException e){
+                    System.out.println("CAGADA");
+                }
+            }
+        }
+        return buffer;
+
+    }
 }
